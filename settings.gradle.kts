33b312b5559ecbf2f8d9a6e4528f5a720f14dd7e
Alex Clement <alexrdclement@gmail.com>
2023-06-19 14:13:22 -0400

    Fix fresh build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,10 @@ dependencyResolutionManagement {
     }
 }
 
+// https://youtrack.jetbrains.com/issue/KTIJ-24981/Gradle-8.-project-sync-fails-with-an-error-No-matching-toolchains-found-for-requested-specification-if-there-is-no-necessary-JDK
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
+
 rootProject.name = "MediaPlayground"
 include(":app")
