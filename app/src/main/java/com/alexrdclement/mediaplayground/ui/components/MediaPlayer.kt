7ea9cae754304be3046d1ddc632e8540cc8095e1
Alex Clement <alexrdclement@gmail.com>
2023-08-20 14:45:05 -0400

    Extract lifecycle event util

--- app/src/main/java/com/alexrdclement/mediaplayground/ui/components/MediaPlayer.kt
+++ app/src/main/java/com/alexrdclement/mediaplayground/ui/components/MediaPlayer.kt
@@ -3,20 +3,14 @@ package com.alexrdclement.mediaplayground.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.common.Player
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
+import com.alexrdclement.mediaplayground.util.rememberLifecycleEvent
 
 @androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
 @Composable
@@ -24,21 +18,7 @@ fun MediaPlayer(
     player: Player,
     modifier: Modifier = Modifier,
 ) {
-    var lifecycle by remember {
-        mutableStateOf(Lifecycle.Event.ON_CREATE)
-    }
-    val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(lifecycleOwner) {
-        val observer = LifecycleEventObserver { _ , event ->
-            lifecycle = event
-        }
-
-        lifecycleOwner.lifecycle.addObserver(observer)
-
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-        }
-    }
+    val lifecycleEvent = rememberLifecycleEvent()
 
     // PlayerView wouldn't directly respect size constraints
     Row(
@@ -54,7 +34,7 @@ fun MediaPlayer(
                 }
             },
             update = { playerView ->
-                when (lifecycle) {
+                when (lifecycleEvent) {
                     Lifecycle.Event.ON_CREATE -> {}
                     Lifecycle.Event.ON_START -> {}
                     Lifecycle.Event.ON_RESUME -> {
