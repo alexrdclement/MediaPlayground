2f2370e8da8c4cffa1df690f55c8840b0242ecf8
Alex Clement <alexrdclement@gmail.com>
2023-12-26 12:48:50 -0500

    Cleanup

--- app/src/main/java/com/alexrdclement/mediaplayground/mediacontrolsheet/MediaControlSheetViewModel.kt
+++ app/src/main/java/com/alexrdclement/mediaplayground/mediacontrolsheet/MediaControlSheetViewModel.kt
@@ -1,27 +1,20 @@
 package com.alexrdclement.mediaplayground.mediacontrolsheet
 
 import androidx.lifecycle.ViewModel
-import androidx.media3.common.Player
 import com.alexrdclement.mediaplayground.mediasession.MediaSessionManager
+import com.alexrdclement.mediaplayground.mediasession.playPause
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MediaControlSheetViewModel @Inject constructor(
-    mediaSessionManager: MediaSessionManager,
+    private val mediaSessionManager: MediaSessionManager,
 ) : ViewModel() {
 
     val loadedMediaItem = mediaSessionManager.loadedMediaItem
     val isPlaying = mediaSessionManager.isPlaying
 
-    private val player: StateFlow<Player?> = mediaSessionManager.player
-
     fun onPlayPauseClick() {
-        if (!isPlaying.value) {
-            player.value?.play()
-        } else {
-            player.value?.pause()
-        }
+        mediaSessionManager.playPause()
     }
 }
