a52e8b07517a2855069c8b2e9cfd266baf768621
Alex Clement <alexrdclement@gmail.com>
2024-02-10 09:41:21 -0500

    Fix build after tv.material3 bump

--- ui-tv/src/main/java/com/alexrdclement/ui/tv/theme/Type.kt
+++ ui-tv/src/main/java/com/alexrdclement/ui/tv/theme/Type.kt
@@ -1,8 +1,10 @@
 package com.alexrdclement.ui.tv.theme
 
+import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Typography
 import com.alexrdclement.ui.shared.theme.bodyLarge
 
+@OptIn(ExperimentalTvMaterial3Api::class)
 val Typography = Typography(
     bodyLarge = bodyLarge,
 )
