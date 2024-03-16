1ad202db1a331d19b54be00b65561fbadd3fe5ea
Alex Clement <alexrdclement@gmail.com>
2024-03-16 14:17:53 -0400

    Fix fixture DI modules

--- media/import/test-fixtures/src/main/kotlin/com/alexrdclement/media/mediaimport/di/FileWriterFixtureModule.kt
+++ media/import/test-fixtures/src/main/kotlin/com/alexrdclement/media/mediaimport/di/FileWriterFixtureModule.kt
@@ -2,13 +2,17 @@ package com.alexrdclement.media.mediaimport.di
 
 import com.alexrdclement.media.mediaimport.fakes.FakeFileWriter
 import com.alexrdclement.mediaplayground.media.mediaimport.FileWriter
+import com.alexrdclement.mediaplayground.media.mediaimport.di.FileWriterModule
 import dagger.Binds
 import dagger.Module
-import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
 
 @Module
-@InstallIn(SingletonComponent::class)
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [FileWriterModule::class],
+)
 abstract class FileWriterFixtureModule {
     @Binds
     abstract fun bindFileWriter(
--- media/import/test-fixtures/src/main/kotlin/com/alexrdclement/media/mediaimport/di/MediaMetadataRetrieverFixtureModule.kt
+++ media/import/test-fixtures/src/main/kotlin/com/alexrdclement/media/mediaimport/di/MediaMetadataRetrieverFixtureModule.kt
@@ -2,13 +2,17 @@ package com.alexrdclement.media.mediaimport.di
 
 import com.alexrdclement.media.mediaimport.fakes.FakeMediaMetadataRetriever
 import com.alexrdclement.mediaplayground.media.mediaimport.MediaMetadataRetriever
+import com.alexrdclement.mediaplayground.media.mediaimport.di.MediaMetadataRetrieverModule
 import dagger.Binds
 import dagger.Module
-import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
 
 @Module
-@InstallIn(SingletonComponent::class)
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [MediaMetadataRetrieverModule::class],
+)
 abstract class MediaMetadataRetrieverFixtureModule {
     @Binds
     abstract fun bindMediaMetadataRetriever(
