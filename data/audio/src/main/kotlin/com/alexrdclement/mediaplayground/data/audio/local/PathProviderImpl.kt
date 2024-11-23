5963a84bb35c077737f39e4261e4e814833d340e
Alex Clement <alexrdclement@gmail.com>
2024-11-23 15:02:10 -0500

    Change imported file write dir to external storage if available, else internal

--- data/audio/src/main/kotlin/com/alexrdclement/mediaplayground/data/audio/local/PathProviderImpl.kt
+++ data/audio/src/main/kotlin/com/alexrdclement/mediaplayground/data/audio/local/PathProviderImpl.kt
@@ -1,6 +1,7 @@
 package com.alexrdclement.mediaplayground.data.audio.local
 
 import android.content.Context
+import android.os.Environment
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.io.files.Path
 import javax.inject.Inject
@@ -8,5 +9,19 @@ import javax.inject.Inject
 class PathProviderImpl @Inject constructor(
     @ApplicationContext private val context: Context,
 ) : PathProvider {
-    override val trackImportFileWriteDir: Path = Path(context.cacheDir.absolutePath)
+    override val trackImportFileWriteDir: Path
+        get() {
+            val defaultPath = Path(context.filesDir.absolutePath)
+
+            if (Environment.getExternalStorageState() != Environment.MEDIA_MOUNTED) {
+                return defaultPath
+            }
+
+            val externalFilesDir = context.getExternalFilesDir(Environment.DIRECTORY_MUSIC)
+            if (externalFilesDir != null) {
+                return Path(externalFilesDir.absolutePath)
+            }
+
+            return defaultPath
+        }
 }
