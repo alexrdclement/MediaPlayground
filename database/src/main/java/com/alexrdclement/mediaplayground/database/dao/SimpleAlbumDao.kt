538e99f650eee9df59cccea193d7adbf8767c821
Alex Clement <alexrdclement@gmail.com>
2025-01-03 16:31:05 -0500

    Manually rewrite getAlbumByTitleAndArtistId query to only include the necessary columns

--- database/src/main/java/com/alexrdclement/mediaplayground/database/dao/SimpleAlbumDao.kt
+++ database/src/main/java/com/alexrdclement/mediaplayground/database/dao/SimpleAlbumDao.kt
@@ -2,7 +2,6 @@ package com.alexrdclement.mediaplayground.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
-import androidx.room.RewriteQueriesToDropUnusedColumns
 import androidx.room.Transaction
 import com.alexrdclement.mediaplayground.database.model.SimpleAlbum
 
@@ -11,11 +10,10 @@ interface SimpleAlbumDao {
     @Transaction
     @Query(
         """
-        SELECT * FROM albums
+        SELECT albums.* FROM albums
         JOIN album_artists ON albums.id = album_artists.album_id
         WHERE albums.title = :title AND album_artists.artist_id = :artistId
         """,
     )
-    @RewriteQueriesToDropUnusedColumns
     suspend fun getAlbumByTitleAndArtistId(title: String, artistId: String): SimpleAlbum?
 }
