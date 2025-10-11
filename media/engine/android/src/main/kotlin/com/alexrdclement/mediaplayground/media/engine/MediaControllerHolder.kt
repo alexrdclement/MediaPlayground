71b3f3fc1faa09b2e07d7dcd48cd34dc8d8ea2bd
Alex Clement <alexrdclement@gmail.com>
2025-10-11 12:05:02 -0400

    Put MediaControllerHolder behind mutex

--- media/engine/android/src/main/kotlin/com/alexrdclement/mediaplayground/media/engine/MediaControllerHolder.kt
+++ media/engine/android/src/main/kotlin/com/alexrdclement/mediaplayground/media/engine/MediaControllerHolder.kt
@@ -7,20 +7,25 @@ import androidx.media3.session.SessionToken
 import com.alexrdclement.mediaplayground.media.session.service.MediaSessionService
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.guava.await
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 class MediaControllerHolder @Inject constructor(
     @ApplicationContext private val context: Context,
 ) {
+    private val mutex = Mutex()
     private var mediaController: MediaController? = null
 
     suspend fun getMediaController(): MediaController {
-        return mediaController ?: createMediaSession().also {
-            this.mediaController = it
+        return mutex.withLock {
+            mediaController ?: createMediaController().also {
+                this.mediaController = it
+            }
         }
     }
 
-    private suspend fun createMediaSession(): MediaController {
+    private suspend fun createMediaController(): MediaController {
         val sessionToken = SessionToken(context, ComponentName(context, MediaSessionService::class.java))
         return MediaController.Builder(context, sessionToken)
             .buildAsync()
