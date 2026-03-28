e748df30bcc22b9f5cca7301222ac43caaaf76da
Alex Clement <alexrdclement@gmail.com>
2026-03-28 19:49:02 -0400

    Add fakes

--- media/engine/test-fixtures/src/main/kotlin/com/alexrdclement/mediaplayground/media/engine/fakes/FakeMediaEngineControl.kt
+++ media/engine/test-fixtures/src/main/kotlin/com/alexrdclement/mediaplayground/media/engine/fakes/FakeMediaEngineControl.kt
@@ -5,8 +5,12 @@ import com.alexrdclement.mediaplayground.media.engine.MediaEngineControl
 class FakeMediaEngineControl(
     override val playlistControl: FakePlaylistControl = FakePlaylistControl(),
     override val transportControl: FakeTransportControl = FakeTransportControl(),
+    override val playheadControl: FakePlayheadControl = FakePlayheadControl(),
+    override val timelineControl: FakeTimelineControl = FakeTimelineControl(),
     override val mediaEngineState: FakeMediaEngineState = FakeMediaEngineState(
         transportControl = transportControl,
         playlistControl = playlistControl,
+        playheadControl = playheadControl,
+        timelineControl = timelineControl,
     ),
 ) : MediaEngineControl
--- media/engine/test-fixtures/src/main/kotlin/com/alexrdclement/mediaplayground/media/engine/fakes/FakeMediaEngineState.kt
+++ media/engine/test-fixtures/src/main/kotlin/com/alexrdclement/mediaplayground/media/engine/fakes/FakeMediaEngineState.kt
@@ -1,17 +1,23 @@
 package com.alexrdclement.mediaplayground.media.engine.fakes
 
 import com.alexrdclement.mediaplayground.media.engine.MediaEngineState
+import com.alexrdclement.mediaplayground.media.engine.PlayheadState
+import com.alexrdclement.mediaplayground.media.engine.TimelineState
 import com.alexrdclement.mediaplayground.media.engine.TransportState
 import kotlinx.coroutines.flow.Flow
 
 class FakeMediaEngineState(
     val transportControl: FakeTransportControl = FakeTransportControl(),
     val playlistControl: FakePlaylistControl = FakePlaylistControl(),
+    val playheadControl: FakePlayheadControl = FakePlayheadControl(),
+    val timelineControl: FakeTimelineControl = FakeTimelineControl(),
 ) : MediaEngineState {
 
     override val playlistState = playlistControl.playlistState
 
-    override fun getTransportState(): Flow<TransportState> {
-        return transportControl.getTransportState()
-    }
+    override fun getTransportState(): Flow<TransportState> = transportControl.getTransportState()
+
+    override fun getPlayheadState(): Flow<PlayheadState> = playheadControl.getPlayheadState()
+
+    override fun getTimelineState(): Flow<TimelineState> = timelineControl.getTimelineState()
 }
